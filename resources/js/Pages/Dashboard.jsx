1fa12124f27071c182a52f5047062af434f67015
studyboyy <apepmvp23@gmai.com>
2023-12-22 11:09:30 +0700

    add fitur

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard({ auth }) {
+    console.log(auth);
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -12,7 +13,7 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <div className="p-6 text-gray-900">You're logged in! {auth.user.email}</div>
                     </div>
                 </div>
             </div>
