e297bbe5673097bbb14bf650c0b138abc7abb10e
studyboyy <apepmvp23@gmai.com>
2023-12-23 10:09:37 +0700

    edit package

--- resources/js/Pages/Admin/Auth/Login.jsx
+++ resources/js/Pages/Admin/Auth/Login.jsx
@@ -1,11 +1,10 @@
 import { Link, Head } from "@inertiajs/react";
 
-
 const Login = () => {
     return (
         <>
             <section className="relative flex h-screen items-center justify-center">
-                <div className="w-full max-w-4xl rounded-lg bg-white p-10 shadow-sm">
+                <div className="w-full max-w-4xl rounded-lg border bg-white p-10 shadow-sm">
                     <div className="grid grid-cols-2 gap-6">
                         <div className="space-y-10">
                             <h1 className="text-4xl font-bold leading-snug">
@@ -18,7 +17,6 @@ const Login = () => {
                                 </Link>
                             </p>
                         </div>
-                       
                     </div>
                 </div>
             </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
@@ -15,9 +15,6 @@ export default {
             fontFamily: {
                 sans: ["GT Walsheim Pro", ...defaultTheme.fontFamily.sans],
             },
-            backgroundImage: {
-                'login-bg' : "url('/images/bg-login.png')",
-            }
         },
     },
 
